e3504f064d6507735cf4f1fb80207ba664071507
caetano991 <miguel.caetano.senai404@gmail.com>
2026-05-05 16:10:01 -0300

    Modal adicionado para acessar descrição

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,89 @@
-import { useState, useEffect } from 'react';
-import {Sidebar} from './componets/Sidebar';
-import {Header} from './componets/Header';
+import { useState, useEffect } from "react";
+import { Sidebar } from "./componets/Sidebar";
+import { Header } from "./componets/Header";
 import {gamesData} from "./data/games";
-import {GameCard} from './componets/GameCard';
+import { GameCard } from "./componets/GameCard";
+import {GameModal} from "./componets/GameModal";
 import AOS from 'aos';
 import "aos/dist/aos.css";
-import './App.css';
+import "./App.css";
 
 function App() {
-    const [search, setSearch] = useState('');
-    const [favorites, setFavorites] = useState([]);
-    const [activeTab, setActiveTab] = useState("dash");
+  const [search, setSearch] =useState ('');
+  const [activeTab, setActiveTab] = useState("dash");
+  const [favorites, setFavorites] = useState([]); //Use states favoritos passo 1
 
-    const filteredGames = gamesData
-    .filter((game) => activeTab === "dash" || favorites.includes(game.id))
-    .filter((game) => game.title.toLowerCase().includes(search.toLowerCase()));
+  const [ selectedGame, setSelectedGame] = useState(null);
 
+  const filteredGames = gamesData
+  .filter ((game) => activeTab === "dash" || favorites.includes(game.id)) // inclui o Id no favoritos passo 2
+  .filter((game)=>game.title.toLowerCase().includes(search.toLowerCase()));
 
-      const toggleFavorites = (id) => {
+  const toggleFavorite = (id) => {
 
-        setFavorites((prev) => prev.includes(id) ? prev.filter((favId) => favId !== id) : [...prev, id])
-
-      }
-
-
-      useEffect(() => {
-        AOS.init({
-          duration: 1000,
-          onde: false,
-          easing: "ease-in-out"
-        })
-      },[]);
+      setFavorites((prev) => prev.includes (id) ? prev.filter((favId) => favId !==id) : [...prev, id],
+     
+     
+      );
+  };
+   
 
 
-  return (
+
+   useEffect(()  => {
+      AOS.init({
+        duration: 1000,
+        once: false,
+        easing: "ease-in-out",
+
+      })
+   },[]);
+
+
+    return (
     <div className="vortex-app">
-      <Sidebar/>
+      <Sidebar activeTab={activeTab} setActiveTab={setActiveTab} />
 
       <main className="vortex-main">
-        <Header search = {search} setSearch = {setSearch} />
+
+        <Header search = {search} setSearch={setSearch} />
+
         <div className="vortex-content">
 
           <h2 className="section-title">
-            {search ? `Resultados para: ${search}` :  "Jogos Recentes"}
+
+            {activeTab === "dash" && "Dashboard"}
+            {activeTab === "favorites" && "Favoritos"}
+            {activeTab === "profile" && "Perfil"}
           </h2>
 
           <div className="vortex-grid">
+       
+         {filteredGames.length > 0 ? (
+          filteredGames.map((g,index) => (
+            <GameCard
+                key={g.id}
+                title = {g.title}
+                category={g.category}
+                banner={g.banner}
+                index ={index}
+                isFavorite= {favorites.includes(g.id)}
+                onFavorite= {() => toggleFavorite(g.id)}
 
-            {filteredGames.length > 0 ? (
-              filteredGames.map((g, index) => (
-                <GameCard 
-                  key={g.id}
-                  title={g.title}
-                  category={g.category}
-                  banner={g.banner}
-                  index={index}
-                  isFavorite={favorites.includes(g.id)}
-                  onFavorite={() => toggleFavorites(g.id)}
-                />
-              ))
-            ): (
-              <p style={{color: "#94a3b8"}}>Nenhum item encontrado...</p>
-            )
-           }
+                onPlay={()=> setSelectedGame(g)}
+              />
+          ))
+         ): (
+         <p style={{color:"#94a3b8"}}> Nenhum jogo encontrado...</p>
+
+         )}
+
+
           </div>
         </div>
       </main>
+      <GameModal game={selectedGame} onClose={()=> setSelectedGame(null)} />
     </div>
-  )
+  );
 }
-
 export default App;
--- src/componets/GameCard.jsx
+++ src/componets/GameCard.jsx
@@ -1,40 +1,36 @@
-import "./Gamecard.css";
+import './GameCard.css';
 import { Heart, Play } from "lucide-react";
 
-export function GameCard({
-  title,
-  category,
-  banner,
-  index,
-  isFavorite,
-  onFavorite,
-}) {
-  return (
-    <div
-      className="vortex-card"
-      data-aos="fade-up"
-      data-aos-delay="{ index * 100 }"
-    >
-      <img src={banner} alt={title} className="card-img" />
+export function GameCard({ title, category, banner, index, isFavorite, onFavorite, onPlay}) {
+    return (
+        <div className="vortex-card"
+        data-aos="fade-up"
+        data-aos-anchor-placement="bottom-bottom" //define efeito
+        data-aos-delay = {index * 50} // define o tempo de animação de cada card
+       
+        >
 
-      <button
-        className={`favorite-btn ${isFavorite ? "active" : ""}`}
-        onClick={onFavorite}
-      >
-        <Heart
-          size={18}
-          fill={isFavorite ? "#7c3aed" : "trasparent"}
-          stroke={isFavorite ? "#7c3aed" : "white"}
-        />
-      </button>
+            <img src={banner} alt={title} className='card-img' />
+           
+            <button className={`favorite-btn ${isFavorite ? "active" : ''}`}
+            onClick={onFavorite}
+            >
+            <Heart
+            size={18}
+            fill={isFavorite ? "#7c3aed" :  "transparent"}
+            stroke={isFavorite ? "#7c3aed" :  "white"}
+            />
+           
+            </button>
 
-      <div className="card-info">
-        <h4>{title}</h4>
-        <p>{category}</p>
-        <button className="play-btn">
-          <Play size={14} fill="white" />
-        </button>
-      </div>
-    </div>
-  );
+            <div className="card-info">
+                <h4>{title}</h4>
+                <p>{category}</p>
+                <button className="play-btn" onClick={onPlay}><Play size={14} fill="white" /></button>
+            </div>
+
+
+
+        </div>
+    )
 }
